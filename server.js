353c3b6715ac3345360fc8a3f6c02478159932c4
yhonaMC <yhonaiker1@gmail.com>
2021-09-03 11:57:06 -0700

    API

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('data.json')
+const middlewares = jsonServer.defaults()
+
+
+server.use(middlewares)
+server.use(router)
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+    console.log('En linea')
+})
